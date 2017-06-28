58dceab45e639d3f78d5bb04879d9c024b467f90
Chris <chris@cypheros.co>
2017-06-28 18:23:12 -0400

    Luna: improve design policy integration
    This redesigns most of launcher3's assets to mock
    Google's Pixel Launcher's design. This is a port of
    https://github.com/amirzaidi/Launcher3/commits/master
    
    - Added G Pill widget
    - Fixed Google Now transitions
    - Removed checks for swipes in apps container
    - Added Date & Time widget view
    - Cleaned up QSB layouts
    - Updated icon and text sizes
    - Imported translations
    - Added support for weather view
    - Added dynamic icon support
    - Updated color scheme resources
    
    Ported from Pixel Launcher by @amirzaidi
    Ported to work with CypherOS's Luna Launcher
    
    Change-Id: I4a09a5e9fa0bb18a439628845e0a506559250786

---
+++ src/com/android/launcher3/pixelui/CustomAppFilter.java
@@ -0,0 +1,19 @@
+package com.android.launcher3.pixelui;
+
+import android.content.ComponentName;
+import java.util.HashSet;
+
+public class CustomAppFilter extends AppFilter
+{
+    private final HashSet mHide;
+
+    public CustomAppFilter() {
+        mHide = new HashSet();
+        mHide.add(ComponentName.unflattenFromString("com.google.android.googlequicksearchbox/.VoiceSearchActivity"));
+        mHide.add(ComponentName.unflattenFromString("com.google.android.apps.wallpaper/.picker.CategoryPickerActivity"));
+    }
+
+    public boolean shouldShowApp(final ComponentName componentName) {
+        return !mHide.contains(componentName);
+    }
+}
