d6971a5c1a6c54b163d8c26f54f8b9f9d3f54492
Chris <chris@cypheros.co>
2017-07-01 07:42:04 -0400

    Luna: add notification badges/dots and widget interface
    Adds support for notification badges to display within application
    icons to give the user indication of an unread notification from
    the application.
    
    Adds support for the notification dots interface from Android O,
    as well as the new widget picker interface.
    
    PS1: This is the generic interface for nougat
    Todo: Implement the Android O interface with Notification Dots.
    PS2: Notification Badges deemed working.To use Notification Badges
    only, use Patchset 2.
    PS3: Intial bringup of Notification Dots and Widget Interface
    
    Change-Id: I52954716495d11e05e2f548f7e8550f91d3b3ce7

--- src/com/android/launcher3/compat/LauncherActivityInfoCompat.java
+++ src/com/android/launcher3/compat/LauncherActivityInfoCompat.java
@@ -18,9 +18,15 @@ package com.android.launcher3.compat;
 
 import android.content.ComponentName;
 import android.content.Context;
+import android.content.Intent;
 import android.content.pm.ApplicationInfo;
+import android.content.pm.LauncherActivityInfo;
+import android.content.pm.LauncherApps;
 import android.content.pm.ResolveInfo;
 import android.graphics.drawable.Drawable;
+import android.os.UserHandle;
+
+import com.android.launcher3.compat.UserHandleCompat;
 
 public abstract class LauncherActivityInfoCompat {
 
@@ -39,5 +45,15 @@ public abstract class LauncherActivityInfoCompat {
      */
     public static LauncherActivityInfoCompat fromResolveInfo(ResolveInfo info, Context context) {
         return new LauncherActivityInfoCompatV16(context, info);
+    }
+	
+	public LauncherActivityInfoCompat(LauncherActivityInfo info) {
+        mLauncherActivityInfo = info;
+    }
+	
+	public static LauncherActivityInfoCompat create(Context context, UserHandle user, Intent intent) {
+        LauncherApps launcherApps = (LauncherApps) context.getSystemService("launcherapps");
+        LauncherActivityInfo info = launcherApps.resolveActivity(intent, user);
+        return new LauncherActivityInfoCompat(info);
     }
 }
