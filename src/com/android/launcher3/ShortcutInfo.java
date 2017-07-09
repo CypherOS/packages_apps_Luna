f712e4f48f786258803299e66292e280ae61c71a
Chris <chris@cypheros.co>
2017-07-08 21:12:59 -0400

    Luna: clean up some code from shortcuts integration
    - Removed dead code
    - Removed unused imports
    - Fixed some lag
    - Fixed shortcut icons not showing unbadged view
    - Change the default notification dot color to white
    
    Contributors: @deletescape
    @paphonbth
    
    Change-Id: I3459a1c2d9c36f6ab9846fb5663023cbee9f93d1

--- src/com/android/launcher3/ShortcutInfo.java
+++ src/com/android/launcher3/ShortcutInfo.java
@@ -158,6 +158,11 @@ public class ShortcutInfo extends ItemInfoWithIcon {
      * Refer {@link #FLAG_RESTORED_ICON}, {@link #FLAG_AUTOINTALL_ICON}
      */
     Intent promisedIntent;
+	
+	/**
+     * Class member variable to display unbadged icons where possible
+     */
+	private Bitmap mUnbadgedIcon;
 
     public ShortcutInfo() {
         itemType = LauncherSettings.BaseLauncherColumns.ITEM_TYPE_SHORTCUT;
@@ -235,6 +240,13 @@ public class ShortcutInfo extends ItemInfoWithIcon {
             updateIcon(iconCache);
         }
         return mIcon;
+    }
+	
+	public Bitmap getUnbadgedIcon(IconCache iconCache) {
+        if (mUnbadgedIcon == null) {
+            return getIcon(iconCache);
+        }
+        return mUnbadgedIcon;
     }
 
     public void updateIcon(IconCache iconCache, boolean useLowRes) {
@@ -326,6 +338,7 @@ public class ShortcutInfo extends ItemInfoWithIcon {
         Bitmap unbadgedBitmap = unbadgedDrawable == null
                 ? cache.getDefaultIcon(Utilities.myUserHandle())
                 : Utilities.createScaledBitmapWithoutShadow(unbadgedDrawable, context);
+		mUnbadgedIcon = unbadgedBitmap;
         setIcon(getBadgedIcon(unbadgedBitmap, shortcutInfo, cache, context));
     }
 
