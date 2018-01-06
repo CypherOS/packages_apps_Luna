27020d1fa8eb4e747d140ff853b255b2d2f7737c
Thecrazyskull <anaskarbila@gmail.com>
2018-01-06 14:57:36 -0500

    Luna: Implementation for gestures
    Adds support for swipe gestures:
    
    - Swipe up for apps
    - Swipe down for search
    
    Also adds support for double tapping to put
    the device to sleep
    
    Change-Id: I339a6e74d8fdcfbd922fd2a6f6164b6ac57488ab

--- Android.mk
+++ Android.mk
@@ -28,11 +28,11 @@ LOCAL_STATIC_JAVA_LIBRARIES := \
     android-support-v7-recyclerview \
     android-support-v7-palette \
     android-support-dynamic-animation
-	
+
 LOCAL_AIDL_INCLUDES := $(LOCAL_PATH)/src
 LOCAL_SRC_FILES := \
     $(call all-java-files-under, src) \
-	$(call all-Iaidl-files-under, src) \
+    $(call all-Iaidl-files-under, src) \
     $(call all-java-files-under, src_config) \
     $(call all-java-files-under, src_flags) \
     $(call all-proto-files-under, protos) \
@@ -81,7 +81,7 @@ LOCAL_STATIC_JAVA_LIBRARIES := \
 LOCAL_AIDL_INCLUDES := $(LOCAL_PATH)/src
 LOCAL_SRC_FILES := \
     $(call all-java-files-under, src) \
-	$(call all-Iaidl-files-under, src) \
+    $(call all-Iaidl-files-under, src) \
     $(call all-java-files-under, src_config) \
     $(call all-java-files-under, go/src_flags) \
     $(call all-proto-files-under, protos) \
