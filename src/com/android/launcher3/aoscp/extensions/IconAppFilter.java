75275d63afe4ebdeeb89808795dd71ba99d1cf1d
Chris <chris@cypheros.co>
2018-02-04 20:45:30 -0500

    Luna: Support custom icon packs
    Allows the user to install and configure custom
    icon packs from the playstore.
    
    @Sirhc95: Adapted to work with Luna Launcher and structure
    
    Change-Id: I137ce5ca701d898bf42690980e9382303f5e8ec8

---
+++ src/com/android/launcher3/aoscp/extensions/IconAppFilter.java
@@ -0,0 +1,27 @@
+package com.android.launcher3.extensions.aoscp;
+
+import android.content.ComponentName;
+import android.content.Context;
+
+import com.android.launcher3.SettingsActivity;
+import com.android.launcher3.Utilities;
+import com.android.launcher3.aoscp.LunaAppFilter;
+
+import co.aoscp.lunalauncher.utils.IconPackUtils;
+
+public class IconAppFilter extends LunaAppFilter {
+    private final Context mContext;
+    private final boolean hasIconPack;
+
+    public IconAppFilter(Context context) {
+        super(context);
+        mContext = context;
+        hasIconPack = !Utilities.getPrefs(context).getString(SettingsActivity.ICON_PACK_PREF, "").isEmpty();
+    }
+
+    @Override
+    public boolean shouldShowApp(ComponentName componentName) {
+        return super.shouldShowApp(componentName) &&
+                (!hasIconPack || !IconPackUtils.isPackProvider(mContext, componentName.getPackageName()));
+    }
+}
