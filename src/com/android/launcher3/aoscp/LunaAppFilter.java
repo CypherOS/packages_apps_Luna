7ff1d6ba7ed68fb1e36a48e6cca9e204191d7235
Chris <chris@cypheros.co>
2018-02-04 22:32:40 -0500

    Luna: Forcefully use custom appfilter
    Work around to force load our app filter since java is being dumb
    
    Change-Id: Ie32b98a2ccc4bb8bcf521d4f66b9d82ce593900d

--- src/com/android/launcher3/aoscp/LunaAppFilter.java
+++ src/com/android/launcher3/aoscp/LunaAppFilter.java
@@ -3,7 +3,6 @@ package com.android.launcher3.aoscp;
 import android.content.ComponentName;
 import android.content.Context;
 
-import com.android.launcher3.AppFilter;
 import com.android.launcher3.SettingsActivity;
 import com.android.launcher3.Utilities;
 
@@ -11,7 +10,7 @@ import co.aoscp.lunalauncher.utils.IconPackUtils;
 
 import java.util.HashSet;
 
-public class LunaAppFilter extends AppFilter {
+public class LunaAppFilter {
 	
     private final HashSet<ComponentName> mHideList = new HashSet<>();
 	
