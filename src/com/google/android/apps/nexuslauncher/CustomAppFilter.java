f4d58c898ab670393cb7faf54496a7abe398f5c5
enricocid <enrico2588@gmail.com>
2018-02-05 23:58:22 -0500

    Luna: implement hidden app feature
    Allow user to hide/show app on the app drawer using multi-selection.
    To do that our custom app filter class will be used.
    By clicking the check menu item selected app will be added to the hide
    list set. On back pressed no changes will be applied.
    
    Updated for Oreo by @Sirhc95
    Also adapted to work with Luna Launcher
    
    Change-Id: I64387fcc399eef0c8d9fe9b4b36c11646143f40c
    Signed-off-by: enricocid <enrico2588@gmail.com>

--- src/com/google/android/apps/nexuslauncher/CustomAppFilter.java
+++ src/com/google/android/apps/nexuslauncher/CustomAppFilter.java
@@ -2,11 +2,15 @@ package com.google.android.apps.nexuslauncher;
 
 import android.content.ComponentName;
 import android.content.Context;
+import android.preference.PreferenceManager;
 
 import com.android.launcher3.SettingsActivity;
 import com.android.launcher3.Utilities;
 
+import java.util.Set;
+
 public class CustomAppFilter extends NexusAppFilter {
+	
     private final Context mContext;
     private final boolean hasIconPack;
 
@@ -18,7 +22,10 @@ public class CustomAppFilter extends NexusAppFilter {
 
     @Override
     public boolean shouldShowApp(ComponentName componentName) {
+		Set<String> hiddenApps = PreferenceManager.getDefaultSharedPreferences(context).getStringSet(Utilities.KEY_HIDDEN_APPS_SET, null);
         return super.shouldShowApp(componentName) &&
-                (!hasIconPack || !CustomIconUtils.isPackProvider(mContext, componentName.getPackageName()));
+		        hiddenApps == null || !hiddenApps.contains(componentName.getPackageName()) &&
+                (!hasIconPack || !CustomIconUtils.isPackProvider(mContext, componentName.getPackageName()))
+				&& !mHideList.contains(componentName.getPackageName());
     }
 }
