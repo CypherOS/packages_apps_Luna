2131920f1e712de3624cdd628d654210fffcd5bc
Chris Crump <chris@cypheros.co>
2019-01-26 15:14:05 -0500

    Luna: Avoid crash when launching from Quickspace
    After 92491a584f441588bc3895d3e0873610b4da03fd make sure we're not doing any extra animation when launching from Quickspace
    
    Change-Id: I9ef750fe63b8d8321e698aa658b4243d7046cd93

--- quickstep/src/com/android/launcher3/LauncherAppTransitionManagerImpl.java
+++ quickstep/src/com/android/launcher3/LauncherAppTransitionManagerImpl.java
@@ -59,6 +59,8 @@ import android.util.Pair;
 import android.view.View;
 import android.view.ViewGroup;
 
+import co.aoscp.lovegood.views.DateTextView;
+
 import com.android.launcher3.DeviceProfile.OnDeviceProfileChangeListener;
 import com.android.launcher3.InsettableFrameLayout.LayoutParams;
 import com.android.launcher3.allapps.AllAppsTransitionController;
@@ -549,7 +551,7 @@ public class LauncherAppTransitionManagerImpl extends LauncherAppTransitionManag
         }
         alpha.setInterpolator(LINEAR);
         appOpenAnimator.play(alpha);
-        if (!(v instanceof BubbleTextView)) {
+        if (!(v instanceof BubbleTextView) || !(v instanceof DateTextView)) {
             BubbleTextView bubbleTextView = (BubbleTextView) v;
             bubbleTextView.setTextVisibility(!windowToIcon && ((BubbleTextView) v).shouldTextBeVisible());
             y = ((BubbleTextView) v).createTextAlphaAnimator(windowToIcon);
